1db5a1ef2e1af7c1a28a18a9cb62c03bb3b5c96f
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-03-24 18:26:24 +0100

    improved plot

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
 figure('canvas')
 
-var handle = plot([0,5],[0,100],'xlabel','Frequency ratio (w/w_n)','ylabel','Amplitude H(w)','title','Forced Vibration response','color',[1,1,1],'axis','fixed')
+var handle = plot([0,3],[0,10],'xlabel','Frequency ratio (w/w_n)','ylabel','Amplitude X/X_static','title','Forced Vibration response','color','white','axis','fixed')
 handle.clf();
 handle.drawAxes();
+handle.lines[0].color="blue";
 var wratio = linspace(0,5)
 var h = zeros(size(wratio))
-var zeta = [0,0.01,0.1,0.2, 1,2]
+var zeta = [0,0.01,0.02,0.05,0.08,0.1,0.15,0.2,0.5,0.75,0.8,1,1.5,2]
 
 function Hofw(wr,z){
     return 1/Math.sqrt( Math.pow(1-wr,2) + Math.pow(2*z*wr,2) ); 
